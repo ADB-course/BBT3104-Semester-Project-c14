d0eebcd85273401ec538e31d6908b8b219e0a75b
Terry <167083271+167146@users.noreply.github.com>
2024-11-21 16:24:20 +0300

    storage
    engines

--- 10_stored_routines_as_functions/script.sql
+++ 10_stored_routines_as_functions/script.sql
@@ -1,1 +1,25 @@
 -- Write your SQL code here
+CREATE FUNCTION BookFlight(
+    IN p_flight_number INT,
+    IN p_passenger_id INT,
+    IN p_seat_number INT
+)
+RETURNS BOOLEAN
+BEGIN
+  DECLARE available_seats INT;
+
+  SELECT AvailableSeats INTO available_seats
+  FROM Flights
+  WHERE FlightNumber = p_flight_number;
+
+  IF available_seats > 0 THEN
+    INSERT INTO Bookings (FlightNumber, PassengerID, SeatNumber, BookingDate)
+    VALUES (p_flight_number, p_passenger_id, p_seat_number, NOW());
+    UPDATE Flights
+    SET AvailableSeats = AvailableSeats - 1
+    WHERE FlightNumber = p_flight_number;
+    RETURN TRUE;
+  ELSE
+    RETURN FALSE;
+  END IF;
+END;
--- 9_stored_routines_as_procedures/script.sql
+++ 9_stored_routines_as_procedures/script.sql
@@ -1,1 +1,25 @@
 -- Write your SQL code here
+CREATE PROCEDURE BookFlight(
+    IN p_flight_number INT,
+    IN p_passenger_id INT,
+    IN p_seat_number INT
+)
+BEGIN
+  DECLARE available_seats INT;
+
+  SELECT AvailableSeats INTO available_seats
+  FROM Flights
+  WHERE FlightNumber = p_flight_number;
+
+  IF available_seats > 0 THEN
+    START TRANSACTION;
+    INSERT INTO Bookings (FlightNumber, PassengerID, SeatNumber, BookingDate)
+    VALUES (p_flight_number, p_passenger_id, p_seat_number, NOW());
+    UPDATE Flights
+    SET AvailableSeats = AvailableSeats - 1
+    WHERE FlightNumber = p_flight_number;
+    COMMIT;
+  ELSE
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Flight is fully booked.';
+  END IF;
+END;
